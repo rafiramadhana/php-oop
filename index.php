e9b7094d4db19bc16efb80c949db63841c253654
Rafi Ramadhana <rf.ramadhana@gmail.com>
2020-11-06 09:17:58 +0700

    #2 Intro to Classes

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+    class User {
+
+        // properties methods
+
+    }
+
+    // instantiating a.k.a. creating a new instance
+    $userOne = new User();
+    $userTwo = new User();
+
+    // get base class
+    echo 'the class is: ' . get_class(($userOne));
+    echo 'the class is: ' . get_class(($userTwo));
+
+?>
+
+<html lang="en">
+<head>
+    <title>PHP Tutorial</title>
+</head>
+<body>
+    
+</body>
+</html>
